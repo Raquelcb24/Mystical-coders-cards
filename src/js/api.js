443df3ce4d80f4e268f709d50e06ce0ecfe94a08
ellisoalgo <ellisjwhitney@gmail.com>
2024-04-24 11:35:47 +0200

    partial api

---
+++ src/js/api.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const createCard = document.querySelector(".js-create-card");
+
+const handleCreate = (event)=>{
+    event.preventDefault();
+    
+
+};
+
+createCard.addEventListener('click', handleCreate)
